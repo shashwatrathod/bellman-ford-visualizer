063e30a5edba76a7a818a164d30a3b47a567ac1c
Shashwat <shashwatrathod22@gmail.com>
2022-05-12 16:38:53 -0400

    added source inout and validations

--- src/components/graph-input/GraphInput.jsx
+++ src/components/graph-input/GraphInput.jsx
@@ -1,7 +1,11 @@
+import clsx from "clsx";
 import React, { useState } from "react";
 
 const GraphInput = () => {
   const [edgesText, setEdgesText] = useState("");
+  const [sourceText, setSourceText] = useState("");
+  const [edgesValid, setEdgesValid] = useState(true);
+  const [sourceValid, setSourceValid] = useState(true);
 
   const nodeNameValid = (nodeName) => {
     const regex = /[a-zA-Z0-9]+/gm;
@@ -13,33 +17,75 @@ const GraphInput = () => {
     return regex.test(edgeWeight);
   };
 
+  const edgesTextInvalid = (_edgesText) => {
+    const lineInvalid = (line) => {
+      const components = line.split(",");
+
+      if (components.length !== 3) return true;
+
+      if (
+        !nodeNameValid(components[0].trim()) ||
+        !nodeNameValid(components[1].trim()) ||
+        !edgeWeightValid(components[2].trim())
+      )
+        return true;
+
+      return false;
+    };
+    const lines = _edgesText.split("\n");
+
+    return lines.some((line) => lineInvalid(line));
+  };
+
+  const edgesTextOnChange = (e) => {
+    const newValue = e.target.value;
+    setEdgesText(newValue);
+
+    if (edgesTextInvalid(newValue)) {
+      setEdgesValid(false);
+    } else {
+      setEdgesValid(true);
+    }
+  };
+
+  const sourceOnChange = (e) => {
+    const newValue = e.target.value;
+    setSourceText(newValue);
+
+    if (!nodeNameValid(newValue)) {
+      setSourceValid(false);
+      return;
+    }
+
+    if (!edgesText.includes(newValue)) {
+      setSourceValid(false);
+      return;
+    }
+
+    setSourceValid(true);
+  };
+
   const onSaveClick = () => {
+    if (!sourceValid || !edgesValid) {
+      return;
+    }
+
+    // Reaching here means that source and edges are already valid. So no need too check again
     const edgesLine = edgesText.split("\n");
     const edges = [];
 
     edgesLine.forEach((line) => {
       const components = line.split(",");
-      if (components.length !== 3) {
-        // Throw some error
-      }
 
       let src = components[0].trim();
       let dest = components[1].trim();
       let weight = components[2].trim();
 
-      if (
-        nodeNameValid(src) &&
-        nodeNameValid(dest) &&
-        edgeWeightValid(weight)
-      ) {
-        edges.push({
-          src,
-          dest,
-          weight: parseInt(weight),
-        });
-      } else {
-        // Throw some error
-      }
+      edges.push({
+        src,
+        dest,
+        weight: parseInt(weight),
+      });
     });
 
     console.log(edges);
@@ -60,16 +106,27 @@ const GraphInput = () => {
       </p>
       <div className="form-floating">
         <textarea
-          className="form-control fs-4"
-          placeholder="Leave a comment here"
+          className={clsx("form-control", "fs-4", {
+            "is-invalid": !edgesValid,
+          })}
           id="graph-edges-input"
           value={edgesText}
-          onChange={(e) => {
-            setEdgesText(e.target.value);
-          }}
+          onChange={edgesTextOnChange}
           style={{ height: "100px" }}
         ></textarea>
         <label htmlFor="graph-edges-input">Edges</label>
+      </div>
+      <div class="form-floating my-2">
+        <input
+          type="text"
+          className={clsx("form-control", "fs-4", {
+            "is-invalid": !sourceValid,
+          })}
+          id="source-input"
+          value={sourceText}
+          onChange={sourceOnChange}
+        />
+        <label htmlFor="source-input">Source Node</label>
       </div>
       <div className="d-grid my-2">
         <button className="btn btn-primary" type="button" onClick={onSaveClick}>
